264a4cc5c267ac26817d42f7a35c079bb57d25c9
Miguel Hurtado <miguelhurtado87@gmail.com>
2011-05-10 11:14:25 +0000

    Moved logout script to system folder

--- cc-content/plugins/HelloWorld/plugin.php
+++ cc-content/plugins/HelloWorld/plugin.php
@@ -3,10 +3,10 @@
 class HelloWorld {
 
     public function Load() {
-        Plugin::Attach ( 'view.body' , array( 'HelloWorld' , 'SayHelloWorld' ) );
+//        Plugin::Attach ( 'view.body' , array( 'HelloWorld' , 'SayHelloWorld' ) );
     }
 
-    static function SayHelloWorld() { echo '<h1>Hello World!</h1>';}
+    static function SayHelloWorld() { echo '<h1>Hello World!</h1>'; }
     static function Info() { /* Info Array */ }
 
 }
