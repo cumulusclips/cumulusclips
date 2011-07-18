22807eaa58f65cef3cb5b6dad51e9caf85068d9b
Miguel Hurtado <miguelhurtado87@gmail.com>
2011-07-18 21:28:07 +0000

    Renamed install config and added ftp fields, added traditional chinese language file

---
+++ install/includes/config.default.php
@@ -0,0 +1,17 @@
+<?php
+
+// Database Credentials
+define ('DB_HOST', '{DB_HOST}');
+define ('DB_USER', '{DB_USER}');
+define ('DB_PASS', '{DB_PASS}');
+define ('DB_NAME', '{DB_NAME}');
+define ('DB_PREFIX', '{DB_PREFIX}');
+
+// FTP Credentials
+define ('FS_METHOD', '{FS_METHOD}');
+define ('FTP_HOST', '{FTP_HOST}');
+define ('FTP_USER', '{FTP_USER}');
+define ('FTP_PASS', '{FTP_PASS}');
+define ('FTP_SSL', '{FTP_SSL}');
+
+?>
