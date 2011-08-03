8a84d75b4f984ce03b2d58d29f82b3859198ad2a
Miguel Hurtado <miguelhurtado87@gmail.com>
2011-08-03 17:34:47 +0000

    Ordered themes by type

--- cc-admin/themes.php
+++ cc-admin/themes.php
@@ -9,6 +9,7 @@
 include ('../cc-core/config/admin.bootstrap.php');
 App::LoadClass ('User');
 App::LoadClass ('Pagination');
+App::LoadClass ('Filesystem');
 
 
 // Establish page variables, objects, arrays, etc
@@ -17,27 +18,31 @@ Plugin::Trigger ('admin.videos.start');
 //$admin = new User ($logged_in);
 $message = null;
 $page_title = 'Themes';
-$header = 'Themes';
 $admin_js[] = ADMIN . '/extras/fancybox/jquery.fancybox-1.3.4.js';
 $admin_js[] = ADMIN . '/js/fancybox.js';
 
 
 ### Handle "Delete" theme if requested
-if (!empty ($_GET['delete']) && !ctype_space ($_GET['delete'])) {
+if (!empty ($_GET['delete']) && !ctype_space ($_GET['delete']) && Functions::ValidTheme ($_GET['delete'])) {
 
-    // Validate theme
-    if (Functions::ValidTheme ($_GET['delete'])) {
+    $theme_path = THEMES_DIR . '/' . $_GET['delete'];
+    $xml = simplexml_load_file ($theme_path . '/theme.xml');
+    if (Settings::Get('active_theme') != $_GET['delete']) {
 
-        $xml = simplexml_load_file (THEMES_DIR . '/' . $_GET['delete'] . '/theme.xml');
-        if (Settings::Get('active_theme') != $_GET['delete']) {
-            // DELETE THEME CODE
-            $message = $xml->name . ' theme has been deleted';
-            $message_type = 'success';
-        } else {
-            $message = 'Active theme cannot be deleted. Activate another theme and then try again';
+        // DELETE THEME CODE
+        $message = $xml->name . ' theme has been deleted';
+        $message_type = 'success';
+        try {
+            Filesystem::Open();
+            Filesystem::Delete ($theme_path);
+        } catch (Exception $e) {
+            $message = $e->getMessage();
             $message_type = 'error';
         }
 
+    } else {
+        $message = 'Active theme cannot be deleted. Activate another theme and then try again';
+        $message_type = 'error';
     }
 
 }
@@ -57,6 +62,30 @@ else if (!empty ($_GET['activate']) && !ctype_space ($_GET['activate'])) {
 }
 
 
+
+// Retrieve themes
+foreach (glob (THEMES_DIR . '/*') as $theme_path) {
+    
+    $theme = new stdClass();
+    $theme->name = basename ($theme_path);
+    if (Functions::ValidTheme ($theme->name)) {
+        $theme->url = HOST . '/cc-content/themes/' . $theme->name;
+        $theme->xml = simplexml_load_file ($theme_path . '/theme.xml');
+
+        // Add theme to corresponding theme type
+        if ($theme->xml->mobile == 'true') {
+            $mobile_themes[] = $theme;
+        } else {
+            $main_site_themes[] = $theme;
+        }
+
+    }
+    
+}
+
+
+
+
 // Output Header
 include ('header.php');
 
@@ -65,53 +94,88 @@ include ('header.php');
 <link rel="stylesheet" type="text/css" href="<?=ADMIN?>/extras/fancybox/jquery.fancybox-1.3.4.css" />
 <div id="themes">
 
-    <h1><?=$header?></h1>
-
+    <!-- BEGIN MAIN SITE THEMES -->
+    <h1>Themes</h1>
 
     <?php if ($message): ?>
     <div class="<?=$message_type?>"><?=$message?></div>
     <?php endif; ?>
 
 
-    <?php foreach (glob (THEMES_DIR . '/*') as $theme_path): ?>
+    <?php foreach ($main_site_themes as $theme): ?>
     
         <div class="block theme">
-            <?php
-            $theme = basename ($theme_path);
-            $theme_url = HOST . '/cc-content/themes/' . $theme;
-            $xml = simplexml_load_file ($theme_path . '/theme.xml');
-            ?>
             
-            <?php if (file_exists ($theme_path . '/screenshot.png')): ?>
-                <div class="screenshot"><img width="200" src="<?=$theme_url?>/screenshot.png" /></div>
-            <?php endif; ?>
+            <div class="screenshot"><img width="200" src="<?=$theme->url?>/screenshot.png" /></div>
 
             <p>
-                <strong><?=$xml->name?></strong>
-                <?php if (!empty ($xml->author)): ?>
-                    by: <strong><?=$xml->author?></strong>
+                <strong><?=$theme->xml->name?></strong>
+                <?php if (!empty ($theme->xml->author)): ?>
+                    by: <strong><?=$theme->xml->author?></strong>
                 <?php endif; ?>
             </p>
 
             
-            <?php if (!empty ($xml->description)): ?>
-                <p><?=$xml->description?></p>
+            <?php if (!empty ($theme->xml->description)): ?>
+                <p><?=$theme->xml->description?></p>
             <?php endif; ?>
 
 
             <p>
-                <?php if ($theme == Settings::Get ('active_theme')): ?>
+                <?php if ($theme->name == Settings::Get ('active_theme')): ?>
                     <strong>Active Theme</strong>
                 <?php else: ?>
-                    <a href="<?=ADMIN?>/themes.php?activate=<?=$theme?>">Activate</a>
-                    &nbsp;&nbsp;|&nbsp;&nbsp; <a href="<?=HOST?>/?preview_theme=<?=$theme?>" class="iframe">Preview</a>
-                    &nbsp;&nbsp;|&nbsp;&nbsp; <a href="<?=ADMIN?>/themes.php?delete=<?=$theme?>">Delete</a>
+                    <a href="<?=ADMIN?>/themes.php?activate=<?=$theme->name?>">Activate</a>
+                    &nbsp;&nbsp;|&nbsp;&nbsp; <a href="<?=HOST?>/?preview_theme=<?=$theme->name?>" class="iframe">Preview</a>
+                    &nbsp;&nbsp;|&nbsp;&nbsp; <a href="<?=ADMIN?>/themes.php?delete=<?=$theme->name?>">Delete</a>
                 <?php endif; ?>
             </p>
 
         </div>
     
     <?php endforeach; ?>
+    <!-- END MAIN SITE THEMES -->
+
+
+
+
+
+
+    <!-- BEGIN MOBILE THEMES -->
+    <h1>Mobile Themes</h1>
+    <?php foreach ($mobile_themes as $theme): ?>
+
+        <div class="block theme">
+
+            <div class="screenshot"><img width="200" src="<?=$theme->url?>/screenshot.png" /></div>
+
+            <p>
+                <strong><?=$theme->xml->name?></strong>
+                <?php if (!empty ($theme->xml->author)): ?>
+                    by: <strong><?=$theme->xml->author?></strong>
+                <?php endif; ?>
+            </p>
+
+
+            <?php if (!empty ($theme->xml->description)): ?>
+                <p><?=$theme->xml->description?></p>
+            <?php endif; ?>
+
+
+            <p>
+                <?php if ($theme->name == Settings::Get ('active_mobile_theme')): ?>
+                    <strong>Active Theme</strong>
+                <?php else: ?>
+                    <a href="<?=ADMIN?>/themes.php?activate=<?=$theme->name?>">Activate</a>
+                    &nbsp;&nbsp;|&nbsp;&nbsp; <a href="<?=HOST?>/?preview_theme=<?=$theme->name?>" class="iframe">Preview</a>
+                    &nbsp;&nbsp;|&nbsp;&nbsp; <a href="<?=ADMIN?>/themes.php?delete=<?=$theme->name?>">Delete</a>
+                <?php endif; ?>
+            </p>
+
+        </div>
+
+    <?php endforeach; ?>
+    <!-- END MOBILE THEMES -->
 
 </div>
 
