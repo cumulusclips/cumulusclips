df7df459c6a13dff8bc6687bb9c45c24043e6781
Miguel Hurtado <miguelhurtado87@gmail.com>
2011-08-05 00:01:20 +0000

    Created Spanish language file, added language management logic

--- cc-admin/languages.php
+++ cc-admin/languages.php
@@ -23,41 +23,92 @@ $admin_js[] = ADMIN . '/js/fancybox.js';
 $active_languages = unserialize (Settings::Get ('active_languages'));
 
 
+### Remove any orphaned languages
+foreach ($active_languages as $key => $language) {
+    $lang_file = DOC_ROOT . '/cc-content/languages/' . $language . '.xml';
+    if (!file_exists ($lang_file)) {
+        unset ($active_languages[$key]);
+    }
+}
+Settings::Set ('active_languages', serialize ($active_languages));
+reset ($active_languages);
+
+
+
+
 ### Handle "Delete" theme if requested
-if (!empty ($_GET['delete']) && !ctype_space ($_GET['delete']) && Functions::ValidTheme ($_GET['delete'])) {
+if (!empty ($_GET['delete']) && !ctype_space ($_GET['delete'])) {
 
-    $theme_path = THEMES_DIR . '/' . $_GET['delete'];
-    $xml = simplexml_load_file ($theme_path . '/theme.xml');
-    if (Settings::Get('active_theme') != $_GET['delete']) {
+    $language_file = DOC_ROOT . '/cc-content/languages/' . $_GET['delete'] . '.xml';
+    if (file_exists ($language_file) && $_GET['delete'] != Settings::Get ('default_language')) {
 
-        // DELETE THEME CODE
-        $message = $xml->name . ' theme has been deleted';
+        // Deactivate language if applicable
+        $key = array_search ($_GET['delete'], $active_languages);
+        if ($key !== false) {
+            unset ($active_languages[$key]);
+            Settings::Set ('active_languages', serialize ($active_languages));
+        }
+        
+        // Delete language file
+        $xml = simplexml_load_file ($language_file);
+        $message = $xml->information->lang_name . ' language has been deleted';
         $message_type = 'success';
         try {
             Filesystem::Open();
-            Filesystem::Delete ($theme_path);
+            Filesystem::Delete ($language_file);
             Filesystem::Close();
         } catch (Exception $e) {
             $message = $e->getMessage();
             $message_type = 'error';
         }
 
-    } else {
-        $message = 'Active theme cannot be deleted. Activate another theme and then try again';
-        $message_type = 'error';
     }
 
 }
 
 
-### Handle "Activate" theme if requested
+### Handle "Activate" language if requested
 else if (!empty ($_GET['activate']) && !ctype_space ($_GET['activate'])) {
 
     // Validate theme
-    if (Functions::ValidTheme ($_GET['activate'])) {
-        $xml = simplexml_load_file (THEMES_DIR . '/' . $_GET['activate'] . '/theme.xml');
-        Settings::Set ('active_theme', $_GET['activate']);
-        $message = $xml->name . ' is now the active theme';
+    $language_file = DOC_ROOT . '/cc-content/languages/' . $_GET['activate'] . '.xml';
+    if (file_exists ($language_file)) {
+        $xml = simplexml_load_file ($language_file);
+        $active_languages[] = $_GET['activate'];
+        Settings::Set ('active_languages', serialize ($active_languages));
+        $message = $xml->information->lang_name . ' has been activated.';
+        $message_type = 'success';
+    }
+
+}
+
+
+### Handle "Deactivate" language if requested
+else if (!empty ($_GET['deactivate']) && !ctype_space ($_GET['deactivate'])) {
+
+    // Validate theme
+    $language_file = DOC_ROOT . '/cc-content/languages/' . $_GET['deactivate'] . '.xml';
+    $key = array_search ($_GET['deactivate'], $active_languages);
+    if ($key !== false) {
+        $xml = simplexml_load_file ($language_file);
+        unset ($active_languages[$key]);
+        Settings::Set ('active_languages', serialize ($active_languages));
+        $message = $xml->information->lang_name . ' has been deactivated.';
+        $message_type = 'success';
+    }
+
+}
+
+
+### Handle "Set Default" language if requested
+else if (!empty ($_GET['default']) && !ctype_space ($_GET['default'])) {
+
+    // Validate language
+    $language_file = DOC_ROOT . '/cc-content/languages/' . $_GET['default'] . '.xml';
+    if (in_array ($_GET['default'], $active_languages) && file_exists ($language_file)) {
+        $xml = simplexml_load_file ($language_file);
+        Settings::Set ('default_language', $_GET['default']);
+        $message = $xml->information->lang_name . ' is now the default language.';
         $message_type = 'success';
     }
 
@@ -75,8 +126,6 @@ foreach (glob (DOC_ROOT . '/cc-content/languages/*') as $language) {
     $lang->xml = simplexml_load_file ($language);
     $lang_list[] = $lang;
 }
-//echo '<pre>',print_r ($lang_list,true),'</pre>';
-//exit();
 
 
 // Output Header
@@ -85,7 +134,7 @@ include ('header.php');
 ?>
 
 <link rel="stylesheet" type="text/css" href="<?=ADMIN?>/extras/fancybox/jquery.fancybox-1.3.4.css" />
-<div id="themes">
+<div id="languages">
 
     <h1>Languages</h1>
 
@@ -118,14 +167,14 @@ include ('header.php');
                 <?php else: ?>
 
                     <?php if ($language->active): ?>
-                        <a href="<?=ADMIN?>/laguage.php?default=<?=$language->filename?>">Set Default</a> &nbsp;&nbsp;|&nbsp;&nbsp;
-                        <a href="<?=ADMIN?>/laguage.php?deactivate=<?=$language->filename?>">Deactivate</a>
+                        <a href="<?=ADMIN?>/languages.php?default=<?=$language->filename?>">Set Default</a> &nbsp;&nbsp;|&nbsp;&nbsp;
+                        <a href="<?=ADMIN?>/languages.php?deactivate=<?=$language->filename?>">Deactivate</a>
                     <?php else: ?>
-                        <a href="<?=ADMIN?>/laguage.php?activate=<?=$language->filename?>">Activate</a>
+                        <a href="<?=ADMIN?>/languages.php?activate=<?=$language->filename?>">Activate</a>
                     <?php endif; ?>
 
                     &nbsp;&nbsp;|&nbsp;&nbsp; <a href="<?=HOST?>/?preview_lang=<?=$language->filename?>" class="iframe">Preview</a>
-                    &nbsp;&nbsp;|&nbsp;&nbsp; <a href="<?=ADMIN?>/laguage.php?delete=<?=$language->filename?>">Delete</a>
+                    &nbsp;&nbsp;|&nbsp;&nbsp; <a href="<?=ADMIN?>/languages.php?delete=<?=$language->filename?>">Delete</a>
                 <?php endif; ?>
             </p>
 
