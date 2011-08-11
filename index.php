4f6d47f788629a72f39a0426880cec790291c938
Miguel Hurtado <miguelhurtado87@gmail.com>
2011-08-11 18:38:14 +0000

    Reverted changes made for testing

--- index.php
+++ index.php
@@ -1,12 +1,1 @@
-<?php
-
-$obj = new stdClass();
-$obj->enabled = '0';
-$obj->host = '';
-$obj->port = 25;
-$obj->username = '';
-$obj->password = '';
-echo serialize($obj);
-
-//include_once (dirname (__FILE__) . '/cc-core/controllers/index.php');
-?>
+<?php include_once (dirname (__FILE__) . '/cc-core/controllers/index.php'); ?>
