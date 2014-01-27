2cbbc329093a2cf8c657e5933bcc3b521ad5cdad
Miguel Hurtado <miguelhurtado87@gmail.com>
2014-01-27 18:28:58 +0000

    Converted edit comments page to use service layer

--- cc-admin/comments_edit.php
+++ cc-admin/comments_edit.php
@@ -1,17 +1,20 @@
 <?php
 
-// Include required files
-include_once (dirname (dirname (__FILE__)) . '/cc-core/config/admin.bootstrap.php');
-App::LoadClass ('User');
-App::LoadClass ('Video');
-App::LoadClass ('Flag');
-App::LoadClass ('Comment');
+// Init application
+include_once(dirname(dirname(__FILE__)) . '/cc-core/config/admin.bootstrap.php');
 
+// Verify if user is logged in
+$userService = new UserService();
+$adminUser = $userService->loginCheck();
+Functions::RedirectIf($adminUser, HOST . '/login/');
+Functions::RedirectIf($userService->checkPermissions('admin_panel', $adminUser), HOST . '/myaccount/');
 
 // Establish page variables, objects, arrays, etc
-Functions::RedirectIf ($logged_in = User::LoginCheck(), HOST . '/login/');
-$admin = new User ($logged_in);
-Functions::RedirectIf (User::CheckPermissions ('admin_panel', $admin), HOST . '/myaccount/');
+$commentMapper = new CommentMapper();
+$commentService = new CommentService();
+$videoMapper = new VideoMapper();
+$videoService = new VideoService();
+$userMapper = new UserMapper();
 $page_title = 'Edit Comment';
 $data = array();
 $errors = array();
@@ -29,12 +32,12 @@ if (!empty ($_SESSION['list_page'])) {
 
 
 ### Verify a record was provided
-if (isset ($_GET['id']) && is_numeric ($_GET['id']) && $_GET['id'] != 0) {
+if (isset ($_GET['id']) && is_numeric ($_GET['id']) && $_GET['id'] > 0) {
 
     ### Retrieve record information
-    $comment = new Comment ($_GET['id']);
-    if ($comment->found) {
-        $video = new Video ($comment->video_id);
+    $comment = $commentMapper->getCommentById($_GET['id']);
+    if ($comment) {
+        $video = $videoMapper->getVideoById($comment->videoId);
     } else {
         header ('Location: ' . ADMIN . '/comments.php');
         exit();
@@ -57,11 +60,11 @@ if (isset ($_POST['submitted'])) {
 
 
     // Validate user fields if anonymous
-    if ($comment->user_id == 0) {
+    if ($comment->userId == 0) {
 
         // Validate Name
         if (!empty ($_POST['name']) && !ctype_space ($_POST['name'])) {
-            $data['name'] = htmlspecialchars ($_POST['name']);
+            $comment->name = trim($_POST['name']);
         } else {
             $errors['name'] = 'Invalid name';
         }
@@ -69,7 +72,7 @@ if (isset ($_POST['submitted'])) {
 
         // Validate Email
         if (!empty ($_POST['email']) && !ctype_space ($_POST['email']) && preg_match ('/^[a-z0-9][a-z0-9_\.\-]+@[a-z0-9][a-z0-9\.\-]+\.[a-z0-9]{2,4}$/i',$_POST['email'])) {
-            $data['email'] = $_POST['email'];
+            $comment->email = trim($_POST['email']);
         } else {
             $errors['email'] = 'Invalid email address';
         }
@@ -77,12 +80,12 @@ if (isset ($_POST['submitted'])) {
 
         // Validate website
         if (!empty ($comment->website) && empty ($_POST['website'])) {
-            $data['website'] = '';
+            $comment->website = null;
         } else if (!empty ($_POST['website']) && !ctype_space ($_POST['website'])) {
             $website = $_POST['website'];
             if (preg_match ('/^(https?:\/\/)?[a-z0-9][a-z0-9\.-]+\.[a-z0-9]{2,4}.*$/i', $website, $matches)) {
                 $website = (empty($matches[1])) ? 'http://' . $website : $website;
-                $data['website'] = htmlspecialchars (trim ($website));
+                $comment->website = trim ($website);
             } else {
                 $errors['website'] = 'Invalid website';
             }
@@ -93,7 +96,7 @@ if (isset ($_POST['submitted'])) {
 
     // Validate status
     if (!empty ($_POST['status']) && !ctype_space ($_POST['status'])) {
-        $data['status'] = htmlspecialchars (trim ($_POST['status']));
+        $newCommentStatus = trim ($_POST['status']);
     } else {
         $errors['status'] = 'Invalid status';
     }
@@ -101,7 +104,7 @@ if (isset ($_POST['submitted'])) {
 
     // Validate comments
     if (!empty ($_POST['comments']) && !ctype_space ($_POST['comments'])) {
-        $data['comments'] = htmlspecialchars (trim ($_POST['comments']));
+        $comment->comments = trim ($_POST['comments']);
     } else {
         $errors['comments'] = 'Invalid comments';
     }
@@ -112,23 +115,26 @@ if (isset ($_POST['submitted'])) {
     if (empty ($errors)) {
 
         // Perform addional actions based on status change
-        if ($data['status'] != $comment->status) {
+        if ($newCommentStatus != $comment->status) {
+            
+            $comment->status = $newCommentStatus;
 
             // Handle "Approve" action
-            if ($data['status'] == 'approved') {
-                $comment->Approve ('approve');
+            if ($comment->status == 'approved') {
+                $commentService->approve($comment, 'approve');
             }
 
             // Handle "Ban" action
-            else if ($data['status'] == 'banned') {
-                Flag::FlagDecision ($comment->comment_id, 'comment', true);
+            else if ($comment->status == 'banned') {
+                $flagService = new FlagService();
+                $flagService->flagDecision($comment, true);
             }
 
         }
 
         $message = 'Comment has been updated';
         $message_type = 'success';
-        $comment->Update ($data);
+        $commentMapper->save($comment);
 
     } else {
         $message = 'The following errors were found. Please correct them and try again.';
@@ -157,7 +163,7 @@ include ('header.php');
 
         <p><a href="<?=$list_page?>">Return to previous screen</a></p>
 
-        <form action="<?=ADMIN?>/comments_edit.php?id=<?=$comment->comment_id?>" method="post">
+        <form action="<?=ADMIN?>/comments_edit.php?id=<?=$comment->commentId?>" method="post">
 
             <div class="row<?=(isset ($errors['status'])) ? ' error' : ''?>">
                 <label>Status:</label>
@@ -169,43 +175,44 @@ include ('header.php');
             </div>
 
             <div class="row"><label>Date Posted:</label>
-                <?=Functions::DateFormat('m/d/Y',$comment->date_created)?>
+                <?=Functions::DateFormat('m/d/Y',$comment->dateCreated)?>
             </div>
 
             <div class="row">
                 <label>In Response To:</label>
-                <a target="_ccsite" href="<?=$video->url?>/"><?=$video->title?></a>
+                <a target="_ccsite" href="<?=$videoService->getUrl($video)?>/"><?=$video->title?></a>
             </div>
 
-            <?php if ($comment->user_id == 0): ?>
+            <?php if ($comment->userId == 0): ?>
 
                 <div class="row<?=(isset ($errors['name'])) ? ' error' : ''?>">
                     <label>*Name:</label>
-                    <input class="text" type="text" name="name" value="<?=(isset ($data['name'])) ? $data['name'] : $comment->name?>" />
+                    <input class="text" type="text" name="name" value="<?=htmlspecialchars($comment->name)?>" />
                 </div>
 
                 <div class="row<?=(isset ($errors['email'])) ? ' error' : ''?>">
                     <label>*Email:</label>
-                    <input class="text" type="text" name="email" value="<?=(isset ($data['email'])) ? $data['email'] : $comment->email?>" />
+                    <input class="text" type="text" name="email" value="<?=htmlspecialchars($comment->email)?>" />
                 </div>
 
                 <div class="row<?=(isset ($errors['website'])) ? ' error' : ''?>">
                     <label>Website:</label>
-                    <input class="text" type="text" name="website" value="<?=(isset ($data['website'])) ? $data['website'] : $comment->website?>" />
+                    <input class="text" type="text" name="website" value="<?=htmlspecialchars($comment->website)?>" />
                 </div>
 
             <?php else: ?>
 
+                <?php $user = $userMapper->getUserById($comment->userId); ?>
                 <div class="row">
                     <label>Username:</label>
-                    <a target="_ccsite" href="<?=$comment->website?>"><?=$comment->name?></a>
+                    <a target="_ccsite" href="<?=HOST . '/members/' . $user->username?>/"><?=$user->username?></a>
                 </div>
 
             <?php endif; ?>
 
             <div class="row<?=(isset ($errors['comment'])) ? ' error' : ''?>">
                 <label>Comments:</label>
-                <textarea rows="7" cols="50" class="text" name="comments"><?=(isset ($data['comments'])) ? $data['comments'] : $comment->comments?></textarea>
+                <textarea rows="7" cols="50" class="text" name="comments"><?=htmlspecialchars($comment->comments)?></textarea>
             </div>
 
             <div class="row-shift">
