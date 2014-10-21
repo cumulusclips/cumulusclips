0d1ca84e05b62a400df35f0587b2684a1d88146f
Miguel Hurtado <miguelhurtado87@gmail.com>
2014-10-21 01:02:16 +0000

    Applies Zend Coding Standard to contact and activate pages

--- cc-core/controllers/activate.php
+++ cc-core/controllers/activate.php
@@ -1,38 +1,36 @@
 <?php
 
-Plugin::Trigger ('activate.start');
+Plugin::trigger('activate.start');
 
 // Verify if user is logged in
 $userService = new UserService();
 $this->view->vars->loggedInUser = $userService->loginCheck();
 
 // Establish page variables, objects, arrays, etc
-Functions::RedirectIf (!$this->view->vars->logged_in, HOST . '/account/');
+Functions::redirectIf(!$this->view->vars->loggedInUser, HOST . '/account/');
 $this->view->vars->message = null;
 
-### Verify token was provided
-if (isset ($_GET['token'])) {
-	
+// Verify token was provided
+if (isset($_GET['token'])) {
     $token = $_GET['token'];
     $userMapper = new UserMapper();
     $user = $userMapper->getUserByCustom(array('confirm_code' => $token, 'status' => 'new'));
     if ($user) {
         $userService->approve($user, 'activate');
-        if (Settings::Get('auto_approve_users') == 1) {
-            $this->view->vars->message = Language::GetText ('activate_success', array ('host' => HOST));
+        if (Settings::get('auto_approve_users') == 1) {
+            $this->view->vars->message = Language::getText('activate_success', array('host' => HOST));
             $_SESSION['user_id'] = $user->userId;
         } else {
-            $this->view->vars->message = Language::GetText ('activate_approve');
+            $this->view->vars->message = Language::getText('activate_approve');
         }
-        $this->view->vars->message_type = 'success';
-        Plugin::Trigger ('activate.activate');
+        $this->view->vars->messageType = 'success';
+        Plugin::trigger('activate.activate');
     } else {
-        $this->view->vars->message = Language::GetText ('activate_error', array ('host' => HOST));
-        $this->view->vars->message_type = 'error';
+        $this->view->vars->message = Language::getText('activate_error', array ('host' => HOST));
+        $this->view->vars->messageType = 'error';
     }
-	
 } else {
     App::Throw404();
 }
 
-Plugin::Trigger ('activate.before_render');
+Plugin::trigger('activate.before_render');
--- cc-core/controllers/contact.php
+++ cc-core/controllers/contact.php
@@ -7,56 +7,53 @@ $userService = new UserService();
 $this->view->vars->loggedInUser = $userService->loginCheck();
 
 // Establish page variables, objects, arrays, etc
-$this->view->vars->Errors = array();
+$this->view->vars->errors = array();
 $this->view->vars->name = null;
 $this->view->vars->email = null;
 $this->view->vars->feedback = null;
 $this->view->vars->message = null;
-$this->view->vars->message_type = null;
+$this->view->vars->messageType = null;
 
-/***********************
-Handle form if submitted
-***********************/
-
-if (isset ($_POST['submitted'])) {
+// Handle form if submitted
+if (isset($_POST['submitted'])) {
 	
     // Validate name
-    if (!empty ($_POST['name']) && !ctype_space ($_POST['name'])) {
-        $this->view->vars->name = trim ($_POST['name']);
+    if (!empty($_POST['name']) && !ctype_space($_POST['name'])) {
+        $this->view->vars->name = trim($_POST['name']);
     } else {
-        $this->view->vars->Errors['name'] = Language::GetText('error_name');
+        $this->view->vars->errors['name'] = Language::getText('error_name');
     }
 
     // Validate email
     $string = '/^[a-z0-9][a-z0-9_\.\-]+@[a-z0-9][a-z0-9\.-]+\.[a-z0-9]{2,4}$/i';
-    if (!empty ($_POST['email']) && !ctype_space ($_POST['email']) && preg_match ($string, $_POST['email'])) {
-        $this->view->vars->email = trim ($_POST['email']);
+    if (!empty($_POST['email']) && !ctype_space($_POST['email']) && preg_match($string, $_POST['email'])) {
+        $this->view->vars->email = trim($_POST['email']);
     } else {
-        $this->view->vars->Errors['email'] = Language::GetText('error_email');
+        $this->view->vars->errors['email'] = Language::getText('error_email');
     }
 
     // Validate feedback
-    if (!empty ($_POST['feedback']) && !ctype_space ($_POST['feedback'])) {
-        $this->view->vars->feedback = trim ($_POST['feedback']);
+    if (!empty($_POST['feedback']) && !ctype_space($_POST['feedback'])) {
+        $this->view->vars->feedback = trim($_POST['feedback']);
     } else {
-        $this->view->vars->Errors['feedback'] = Language::GetText('error_message');
+        $this->view->vars->errors['feedback'] = Language::getText('error_message');
     }
 
     // Send email if no errors
-    if (empty ($this->view->vars->Errors)) {
+    if (empty($this->view->vars->errors)) {
         $subject = 'Message received From ' . $config->sitename;
         $Msg = "Name: " . $this->view->vars->name . "\n";
         $Msg .= "E-mail: " . $this->view->vars->email . "\n";
         $Msg .= "Message:\n" . $this->view->vars->feedback;
-        App::Alert ($subject, $Msg);
+        App::alert($subject, $Msg);
         Plugin::triggerEvent('contact.send');
 
-        $this->view->vars->message_type = 'success';
-        $this->view->vars->message = Language::GetText('success_contact_sent');
+        $this->view->vars->messageType = 'success';
+        $this->view->vars->message = Language::getText('success_contact_sent');
     } else {
-        $this->view->vars->message_type = 'errors';
-        $this->view->vars->message = Language::GetText('errors_below');
-        $this->view->vars->message .= '<br /><br /> - ' . implode ('<br /> - ', $this->view->vars->Errors);
+        $this->view->vars->messageType = 'errors';
+        $this->view->vars->message = Language::getText('errors_below');
+        $this->view->vars->message .= '<br /><br /> - ' . implode('<br /> - ', $this->view->vars->errors);
     }
 }
 
