75e89a174c3ce9f8d4caf4a39af360f955c0b3d6
Miguel Hurtado <miguelhurtado87@gmail.com>
2014-11-28 21:10:28 +0000

    Removes 'remember me' feature, to be replaced with 'keep me logged in' in a later release

--- cc-core/controllers/login.php
+++ cc-core/controllers/login.php
@@ -7,6 +7,7 @@ $userService = new UserService();
 $this->view->vars->loggedInUser = $userService->loginCheck();
 Functions::redirectIf(!$this->view->vars->loggedInUser, HOST . '/account/');
 
+$config = Registry::get('config');
 $this->view->vars->username = null;
 $this->view->vars->password = null;
 $this->view->vars->message = null;
@@ -14,27 +15,18 @@ $this->view->vars->message_type = null;
 $this->view->vars->login_submit = null;
 $this->view->vars->forgot_submit = null;
 
-// User Requested forgot password
-if (isset ($_GET['action']) && $_GET['action'] == 'forgot') {
-    $this->view->vars->forgot_submit = true;
-}
+// Handle login form if submitted
+if (isset($_POST['submitted_login'])) {
 
-/*****************************
-Handle login form if submitted
-*****************************/
-
-if (isset ($_POST['submitted_login'])) {
-
-	
     $this->view->vars->login_submit = true;
 
     // validate username
-    if (!empty ($_POST['username']) && !ctype_space ($_POST['username'])) {
+    if (!empty($_POST['username'])) {
         $this->view->vars->username = trim($_POST['username']);
     }
 
     // validate password
-    if (!empty ($_POST['password']) && !ctype_space ($_POST['password'])) {
+    if (!empty($_POST['password'])) {
         $this->view->vars->password = $_POST['password'];
     }
 	
@@ -42,44 +34,34 @@ if (isset ($_POST['submitted_login'])) {
     if ($this->view->vars->username && $this->view->vars->password) {
 
         if ($userService->login($this->view->vars->username, $this->view->vars->password)) {
-
-            if (isset ($_POST['remember']) && $_POST['remember'] == 'true') {
-                setcookie ('username',$this->view->vars->username,time()+60*60*24*9999,'/','',0);
-                setcookie ('password',$this->view->vars->password,time()+60*60*24*9999,'/','',0);
-                Plugin::triggerEvent('login.remember_me');
-            }
             Plugin::triggerEvent('login.login');
-            header ('Location: ' . HOST . '/account/');
-
+            header('Location: ' . HOST . '/account/');
         } else {
-            $this->view->vars->message = Language::GetText('error_invalid_login');
+            $this->view->vars->message = Language::getText('error_invalid_login');
             $this->view->vars->message_type = 'errors';
             $this->view->vars->login_submit = null;
         }
 
     } else {
-        $this->view->vars->message = Language::GetText('error_general');
+        $this->view->vars->message = Language::getText('error_general');
         $this->view->vars->message_type = 'errors';
     }
 }
 
-/***********************
-Handle forgot login form
-***********************/
-
-if (isset ($_POST['submitted_forgot'])) {
+// Handle forgot login form
+if (isset($_POST['submitted_forgot'])) {
 	
     $this->view->vars->forgot_submit = true;
 
     // validate email
     $string = '/^[a-z0-9][a-z0-9\._-]+@[a-z0-9][a-z0-9\.-]+[a-z0-9]{2,4}$/i';
-    if (!empty ($_POST['email']) && !ctype_space ($_POST['email']) && preg_match ($string,$_POST['email'])) {
+    if (!empty($_POST['email']) && preg_match($string,$_POST['email'])) {
 
         $userMapper = new UserMapper();
         $user = $userMapper->getUserByCustom(array('email' => $_POST['email'], 'status' => 'active'));
         if ($user) {
             $new_password = $userService->resetPassword($user);
-            $this->view->vars->message = Language::GetText('success_login_sent');
+            $this->view->vars->message = Language::getText('success_login_sent');
             $this->view->vars->message_type = 'success';
             $this->view->vars->forgot_submit = null;
 
@@ -93,12 +75,12 @@ if (isset ($_POST['submitted_forgot'])) {
             $mail->Send ($user->email);
             Plugin::triggerEvent('login.password_reset');
         } else {
-            $this->view->vars->message = Language::GetText('error_no_users_email');
+            $this->view->vars->message = Language::getText('error_no_users_email');
             $this->view->vars->message_type = 'errors';
         }
 
     } else {
-        $this->view->vars->message = Language::GetText('error_email');
+        $this->view->vars->message = Language::getText('error_email');
         $this->view->vars->message_type = 'errors';
     }
 }
