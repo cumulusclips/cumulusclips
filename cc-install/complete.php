3ad0d5a8c46588e6f8e4a4dc32f0dcdb1489c486
Miguel Hurtado <miguelhurtado87@gmail.com>
2014-11-30 14:15:28 +0000

    Fixes bug in installer entry to first run

--- cc-install/complete.php
+++ cc-install/complete.php
@@ -35,7 +35,7 @@ $result = @mysql_query ($query);
 $query = "INSERT INTO " . $settings->db_prefix . "users (username, password, email, date_created, status, role, released) VALUES";
 $query .= "('$settings->admin_username', '" . md5 ($settings->admin_password) . "', '$settings->admin_email', NOW(), 'active', 'admin', 1)";
 $result = @mysql_query ($query);
-$id = mysql_insert_id();
+$id = @mysql_insert_id();
 
 // Save admin user's privacy settings
 $query = "INSERT INTO " . $settings->db_prefix . "privacy (user_id) VALUES ($id)";
@@ -50,6 +50,5 @@ $query = "INSERT INTO " . $settings->db_prefix . "playlists (user_id, public, ty
 $result = @mysql_query($query);
 
 // Log user into admin panel
-$id = @mysql_insert_id();
 $_SESSION['loggedInUserId'] = $id;
 header ("Location: " . $settings->base_url . '/cc-admin/?first_run');
