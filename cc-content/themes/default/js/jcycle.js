843c30a2f2ae25b16c6c6b8ddd380e43680e5dde
Miguel Hurtado <miguelhurtado87@gmail.com>
2014-12-06 16:30:58 +0000

    Fixes broken featured slides in home page slideshow

--- cc-content/themes/default/js/jcycle.js
+++ cc-content/themes/default/js/jcycle.js
@@ -4,10 +4,10 @@ $(document).ready(function(){
         pager:'#slideshow_nav',
         timeout:6000,
         before:function(currSlideElement, nextSlideElement, options, forwardFlag){
-            setTimeout ( function(){$(nextSlideElement).find('.slide_text').animate({height:'show'})} , 1500 );
+            setTimeout ( function(){$(nextSlideElement).find('.slide-text').animate({height:'show'})} , 1500 );
         },
         after:function(currSlideElement, nextSlideElement, options, forwardFlag){
-            $(currSlideElement).find('.slide_text').hide();
+            $(currSlideElement).find('.slide-text').hide();
         }
     });
 });
